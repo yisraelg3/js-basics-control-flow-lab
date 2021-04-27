15591a92ba5681c6f3bdc51c4a0028daa401af4a
Yisrael Gurkow <yisrelg3@gmail.com>
2021-04-26 23:34:39 -0400

    commit tree ac90164150ef6449381aa92f67d21edb1dfbb69e

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } else if (feet > 2000 && feet < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+  } else {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+  switch (tipAmount) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+   
 }
